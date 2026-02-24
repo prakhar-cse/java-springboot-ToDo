fe63411717b9c63d0561404e2878da7b37e23bf5
prakhar-cse <prakhar.cse.gbu@gmail.com>
2026-02-25 02:12:14 +0530

    init

---
+++ src/main/java/com/practice/java_springboot_ToDo/JavaSpringbootToDoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.java_springboot_ToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootToDoApplication.class, args);
+	}
+
+}
