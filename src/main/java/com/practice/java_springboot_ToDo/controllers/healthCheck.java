47ad3d0d5b9bcba173ceb40c9d2b945b32708f21
prakhar-cse <prakhar.cse.gbu@gmail.com>
2026-02-28 02:15:40 +0530

    added health check

--- src/main/java/com/practice/java_springboot_ToDo/controllers/healthCheck.java
+++ src/main/java/com/practice/java_springboot_ToDo/controllers/healthCheck.java
@@ -1,11 +1,15 @@
 package com.practice.java_springboot_ToDo.controllers;
 
-@RestController("health")
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("health")
 public class healthCheck {
-
-    @GetMapping()
-    public void healthCheck(){
-
+    @GetMapping
+    public String healthCheck(){
+        return "Service is healthy";
     }
 
 }
